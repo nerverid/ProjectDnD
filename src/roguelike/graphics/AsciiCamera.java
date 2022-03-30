a80a575d9c04d3d42a7a627e9f1898f151f4acbc
Krivko Konstantin <nero-add@mail.ru>
2022-03-30 17:29:35 +0700

    Add main Class

--- src/roguelike/graphics/AsciiCamera.java
+++ src/roguelike/graphics/AsciiCamera.java
@@ -1,5 +1,13 @@
 package roguelike.graphics;
 
-public class AsciiCamera {
+import java.awt.*;
 
+public class AsciiCamera {
+	
+	int screenWidth;
+	int screenHeight;
+
+	public AsciiCamera(Rectangle Bounds, Rectangle viewArea) {
+		
+	}
 }
