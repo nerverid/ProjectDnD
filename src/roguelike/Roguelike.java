fb99fa936eb96a9589c6b1a33b95f00e32223eb1
Konstantin <nero-add@mail.ru>
2022-04-27 15:44:38 +0700

    Edit WorlBuilder class

--- src/roguelike/Roguelike.java
+++ src/roguelike/Roguelike.java
@@ -1,6 +1,11 @@
 package roguelike;
 
 import java.awt.Rectangle;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.Map;
 
 import roguelike.entities.Creature;
@@ -35,7 +40,45 @@ public class Roguelike {
 		this.screenWidth = screenWidth;
 		this.screenHeight = screenHeight;
 		
+		gameViewArea = new Rectangle(screenWidth, screenHeight-5);
+		
 		ui = new HelpPanel(screenWidth, screenHeight, new Rectangle(mapWidth, mapHeight));
+		
+		creatureData = loadData(Paths.get("src", "roguelike", "creature.txt").toString());
+		tileData = loadData(Paths.get("src", "roguelike", "tiles.txt").toString());
+		itemData = loadData(Paths.get("src", "rouguelike", "item.txt").toString());
+		
+		createWorld();
+	}
+	
+	public Map<String, Map<String, String>> loadData(String file){
+		Map<String, Map<String, String>> entityMap = new HashMap<>();
+		String line = "";
+		String [] attributeNames = new String[10];
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+			line = br.readLine();
+			
+			if(line != null) {
+				attributeNames = line.split(", ");
+			}
+			
+			while((line = br.readLine()) != null) {
+				String [] data = line.split(", ");
+				Map<String, String> entityData = new HachMap<>();
+				
+				for(int i=0; i<attributeNames.length; i++) {
+					entityDate.put(attributeNames[i], data[i]);
+				}
+				
+				String name = data[1];
+				entityMap.put(name, entityData);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return entityMap;
 	}
 	
 	public static void main(String [] args) {
